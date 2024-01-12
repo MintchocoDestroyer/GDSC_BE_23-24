99e4dae03ac1963326c0e06155019e8b8044e24e
황제연 <kingpele9018@gmail.com>
2024-01-12 16:23:37 +0900

    Delete gdsc directory

--- gdsc/src/main/java/com/example/gdsc/data/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gdsc.data.repository;
-
-import com.example.gdsc.data.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
