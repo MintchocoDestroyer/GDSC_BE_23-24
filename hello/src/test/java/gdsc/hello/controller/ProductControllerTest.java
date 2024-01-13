d06f73d84db413df4c994bcaef1c709a14a47911
hwangjeyeon <kingpele9018@gmail.com>
2024-01-13 22:38:56 +0900

    Test: ProductController 컨트롤러 Mock 테스트

--- hello/src/test/java/gdsc/hello/controller/ProductControllerTest.java
+++ hello/src/test/java/gdsc/hello/controller/ProductControllerTest.java
@@ -10,9 +10,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(ProductController.class)
@@ -28,11 +31,24 @@ class ProductControllerTest {
     @DisplayName("MockMvc를 통한 Product 데이터 가져오기 테스트")
     void getProductTest() throws Exception{
         given(productService.getProduct(123L)).willReturn(
-                new ProductResponseDto(123L, "pen", 5000, 2000)
-        );
+                new ProductResponseDto(123L, "pen", 5000, 2000));
 
+        String productId = "123";
 
+        mockMvc.perform(
+                        get("/product?number=" + Long.parseLong(productId)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(
+                        "$.number").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
 
+        verify(productService).getProduct(123L);
     }
+
+
+
 
 }
