029489c555f24c59e16892621ba7900fdc37966f
hwangjeyeon <kingpele9018@gmail.com>
2024-01-13 23:00:41 +0900

    Fix: createProduct 메소드 테스트 실패 문제 해결을 위한 버그 수정

--- hello/src/main/java/gdsc/hello/data/dto/ProductDto.java
+++ hello/src/main/java/gdsc/hello/data/dto/ProductDto.java
@@ -1,9 +1,15 @@
 package gdsc.hello.data.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class ProductDto {
 
     private String name;
