066207494dc317dd35d940f894280e93ca483cd2
hwangjeyeon <kingpele9018@gmail.com>
2024-02-03 23:54:10 +0900

    Feat: Validated 학습 기능 및 Telphone 애노테이션 구현

---
+++ hello/src/main/java/gdsc/hello/validate/dto/ValidatedRequestDto.java
@@ -0,0 +1,38 @@
+package gdsc.hello.validate.dto;
+
+import gdsc.hello.config.annotation.Telephone;
+import gdsc.hello.validate.group.ValidationGroup1;
+import gdsc.hello.validate.group.ValidationGroup2;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ValidatedRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Telephone
+    private String phoneNumber;
+
+    @Min(value = 20, groups = ValidationGroup1.class)
+    @Max(value = 40, groups = ValidationGroup1.class)
+    private int age;
+
+    @Size(min = 0, max = 40)
+    private String description;
+
+    @Positive(groups = ValidationGroup2.class)
+    private int count;
+
+    @AssertTrue
+    private boolean booleanCheck;
+
+}
